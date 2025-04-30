33db9ad68ee1ab293084d52f1a13d7b240eda50b
surbhi930 <surbhi93930@gmail.com>
2025-04-30 18:41:12 -0400

    UNET model

--- models.py
+++ models.py
@@ -92,3 +92,58 @@ def NDWS_CAE(input_shape):
 
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
     return model
+
+# --------------------------
+# U-Net Architecture
+# --------------------------
+
+def UNET(input_shape):
+    inputs = layers.Input(shape=input_shape)
+
+    # Encoder (downsampling path)
+    c1 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+    c1 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(c1)
+    p1 = layers.MaxPooling2D((2, 2))(c1)
+
+    c2 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(p1)
+    c2 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(c2)
+    p2 = layers.MaxPooling2D((2, 2))(c2)
+
+    c3 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(p2)
+    c3 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(c3)
+    p3 = layers.MaxPooling2D((2, 2))(c3)
+
+    c4 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(p3)
+    c4 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(c4)
+    p4 = layers.MaxPooling2D((2, 2))(c4)
+
+    # Bottleneck
+    c5 = layers.Conv2D(1024, (3, 3), activation='relu', padding='same')(p4)
+    c5 = layers.Conv2D(1024, (3, 3), activation='relu', padding='same')(c5)
+
+    # Decoder (upsampling path)
+    u6 = layers.UpSampling2D((2, 2))(c5)
+    u6 = layers.concatenate([u6, c4])  # skip connection
+    c6 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(u6)
+    c6 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(c6)
+
+    u7 = layers.UpSampling2D((2, 2))(c6)
+    u7 = layers.concatenate([u7, c3])
+    c7 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(u7)
+    c7 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(c7)
+
+    u8 = layers.UpSampling2D((2, 2))(c7)
+    u8 = layers.concatenate([u8, c2])
+    c8 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(u8)
+    c8 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(c8)
+
+    u9 = layers.UpSampling2D((2, 2))(c8)
+    u9 = layers.concatenate([u9, c1])
+    c9 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(u9)
+    c9 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(c9)
+
+    # Output layer
+    outputs = layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
+
+    model = Model(inputs, outputs)
+    return model
